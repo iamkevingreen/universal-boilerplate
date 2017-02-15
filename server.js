7f3df4c22dca361195a3cef1b92a2c2f8b7cf763
iamkevingreen <kevin@redantler.com>
2017-02-15 17:34:23 -0500

    add the webpack dashboard to the dev process

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ import express from 'express';
 import webpack from 'webpack';
 import middleware from './src/middleware';
 
+import DashboardPlugin from 'webpack-dashboard/plugin';
+
 const app = express();
 
 if(process.env.NODE_ENV === 'development') {
 	const config = require('./webpack.config.dev');
 	const compiler = webpack(config);
+	compiler.apply(new DashboardPlugin());
 	app.use(require('webpack-dev-middleware')(compiler, {
 		noInfo: true,
 		publicPath: config.output.publicPath,
