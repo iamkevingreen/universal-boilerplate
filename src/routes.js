e5e16642116ab3bdcce22f39a593e088a70ca871
iamkevingreen <kevin@redantler.com>
2017-02-15 18:09:30 -0500

    wrap the boiler plate in a basic layout component

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
+
+import App from './components/layout';
 import Home from './components/home';
 import Page from './components/page';
 
 export default (
-	<Route path='/'>
+	<Route path='/' component={App}>
 		<IndexRoute component={Home} />
 		<Route path='page' component={Page} />
 	</Route>
